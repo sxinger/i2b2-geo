ebe87c3ae4587d711042850a6ed39e0151ffad50
Dan Connolly <dconnolly@kumc.edu>
2017-01-04 10:03:22 -0600

    connect() should return a connection, not an engine

--- geo_addr_stage.py
+++ geo_addr_stage.py
@@ -293,7 +293,7 @@ if __name__ == '__main__':
             logging.basicConfig(level=logging.INFO,
                                 format=format, datefmt=datefmt)
             db_url = environ[db_label]
-            return create_engine(db_url)
+            return create_engine(db_url).connect()
 
         main(argv,
              cwd=Path('.', (io_open, path_join)),
