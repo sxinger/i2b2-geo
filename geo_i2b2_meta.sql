d2fd385cb8cb1d8a83bd5d37033117a3f7d6d05a
sxinger <sxingerl@gmail.com>
2020-06-05 03:12:58 -0500

    convert to i2b2 metadata format

--- geo_i2b2_meta.sql
+++ geo_i2b2_meta.sql
@@ -1,6 +1,5 @@
 /*****************************************/
-/*construct a geo_dimension in EAV format*/
-/*i2b2 metadata                          */
+/*map ACS concepst to i2b2 metadata      */
 /*****************************************/
 
 /*ACS ontology*/
@@ -52,15 +51,15 @@ union all
 select 3 c_hlevel
      , 'LA' c_visualattributes
      , ('RUCA:' || ruca1) c_basecode
-     , case when ruca1 = 1 then 'Metropolitan area core: primary flow within an Urbanized Area (UA)',
-            when ruca1 = 2 then 'Metropolitan area high commuting: primary flow 30% or more to a UA',
-            when ruca1 = 3 then 'Metropolitan area low commuting: primary flow 10% to 30% to a UA',
-            when ruca1 = 4 then 'Micropolitan area core: primary flow within an Urban Cluster (UC) of 10,000 through 49,999 (large UC)',
-            when ruca1 = 5 then 'Micropolitan high commuting: primary flow 30% or more to a large UC',
-            when ruca1 = 6 then 'Micropolitan low commuting: primary flow 10% to 30% to a large UC',
-            when ruca1 = 7 then 'Small town core: primary flow within an Urban Cluster of 2,500 through 9,999 (small UC)',
-            when ruca1 = 8 then 'Small town high commuting: primary flow 30% or more to a small UC',
-            when ruca1 = 9 then 'Small town low commuting: primary flow 10% through 29% to a small UC',
+     , case when ruca1 = 1 then 'Metropolitan area core: primary flow within an Urbanized Area (UA)'
+            when ruca1 = 2 then 'Metropolitan area high commuting: primary flow 30% or more to a UA'
+            when ruca1 = 3 then 'Metropolitan area low commuting: primary flow 10% to 30% to a UA'
+            when ruca1 = 4 then 'Micropolitan area core: primary flow within an Urban Cluster (UC) of 10,000 through 49,999 (large UC)'
+            when ruca1 = 5 then 'Micropolitan high commuting: primary flow 30% or more to a large UC'
+            when ruca1 = 6 then 'Micropolitan low commuting: primary flow 10% to 30% to a large UC'
+            when ruca1 = 7 then 'Small town core: primary flow within an Urban Cluster of 2,500 through 9,999 (small UC)'
+            when ruca1 = 8 then 'Small town high commuting: primary flow 30% or more to a small UC'
+            when ruca1 = 9 then 'Small town low commuting: primary flow 10% through 29% to a small UC'
             when ruca1 = 10 then 'Rural areas: primary flow to a tract outside a UA or UC (including self)'
             else 'undefined'
        end as c_name
@@ -80,12 +79,13 @@ group by SUBJECTGRP, SUBJECTNAME
 
 union all
 
-select HLEVEL+2 c_hlevel
+select distinct
+       HLEVEL+2 c_hlevel
       ,NODE_POS c_visualattributes
-      ,('ACS|' || TABLE_ID || ':' || VARIABLE_CODE || '_' ||GEO_LEVEL) c_basecode
+      ,('ACS|' || TABLE_ID || ':' || VARIABLE_CODE) c_basecode
       ,VARIABLE_LABEL c_name
       ,(SUBJECTGRP || '-' || SUBJECTNAME || '\' || VARIABLE_LABEL) path
-from acs_meta
+from acs_metadata
 )
 ;
 
